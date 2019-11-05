115162720181815a400227921bc91a8c218b4590
Joanna <j_kaznocha@tlen.pl>
2019-11-05 16:34:04 +0100

    create index.html and style.css

---
+++ main.js
@@ -0,0 +1,36 @@
+const colors = ["lightyellow", "lightseagreen", "lightgreen", "lightblue", "lightcoral", "lightviolet", "lightgrey", "white", "lightpink"],
+    div = document.querySelectorAll('div');
+
+// const blocks = [...div];
+
+
+
+// const randomNumber = () => {
+
+//     const number = Math.floor(Math.random() * 9),
+//         number2 = Math.floor(Math.random() * 9)
+//     color1 = colors[number];
+//     color2 = colors[number2];
+//     //  return (color1, color2)
+//     console.log(color1, color2)
+// }
+
+// const startGame = () => {
+
+//     for (i = 0; i <= 9; i++) {
+//         randomNumber();
+
+//         blocks.forEach(block => block.style.backgroundColor = color1)
+//     };
+//     for (i = 0; 18 >= i > 9; i++) {
+//         randomNumber();
+
+//         blocks.forEach(block => block.style.backgroundColor = color2)
+//     };
+
+//     return blocks
+// }
+
+// window.addEventListener('load', startGame)
+
+
